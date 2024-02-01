62891574482a5fa972993fc1305e5ef4768be838
Christian Troy Andrada <63133539+christiantroyandrada@users.noreply.github.com>
2024-02-01 22:13:30 +0800

    Update AboutView.vue
    To add more description on why this side project existed back then in the first place.  And to learn how to code fullstack in JS + MongoDB technology

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>About this App</h1>
+    <h5>Created by: Christian Troy Andrada</h5>
+    <h5>Purpose: To learn how to integrate Vue and node.js technology with MongoDB</h5>
   </div>
 </template>
